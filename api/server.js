5ca01c882c22ffc421ead040521d562b52ea6108
Wisnu Yumna Yudhanta <44568540+niceProg@users.noreply.github.com>
2025-07-11 01:09:21 +0700

    update fix build vercel

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const geminiRoute = require("../routes/gemini");
-app.use("/api", geminiRoute);
+app.use("/", geminiRoute);
 
 module.exports = serverless(app);
 
