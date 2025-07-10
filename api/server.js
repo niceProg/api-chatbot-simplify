cc7a0f50e706b1cdf10996af96d7ffe30a886742
Wisnu Yumna Yudhanta <44568540+niceProg@users.noreply.github.com>
2025-07-11 01:01:43 +0700

    update fix build vercel

--- api/server.js
+++ api/server.js
@@ -5,13 +5,15 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT
+// const PORT = process.env.PORT        //Untuk Development
 app.use(cors());
 app.use(bodyParser.json());
 
 const geminiRoute = require("../routes/gemini");
 app.use("/api", geminiRoute);
 
-module.exports = serverless(app.listen(PORT, () => {
-     console.log(`Server running on port ${PORT}`);
-}));
+module.exports = serverless(app);
+
+// module.exports = serverless(app.listen(PORT, () => {
+//      console.log(`Server running on port ${PORT}`);
+// }));
